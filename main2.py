2bd47b027237687314d7f7adf1ddca90ddac6e94
Rupesh Sarvade <85817784+RupeshSarvade@users.noreply.github.com>
2023-04-01 07:47:08 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,8 @@
 import moviepy.editor as mpe
 
-video = mpe.VideoFileClip('data/3.mp4').subclip(0,15).without_audio()
-audio_bg = mpe.AudioFileClip("data/audio.mp3")
+video = mpe.VideoFileClip('data/3.mp4').subclip(0,15).without_audio() #import video
+
+audio_bg = mpe.AudioFileClip("data/audio.mp3") #import audio
 # final_audio = mpe.CompositeAudioClip([audio_bg, video.audio])
-final_clip = video.set_audio(audio_bg)
-final_clip.write_videofile("output.mp4")
+final_clip = video.set_audio(audio_bg) # combine them
+final_clip.write_videofile("output.mp4") #save output
